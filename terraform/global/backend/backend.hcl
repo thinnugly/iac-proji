370cdf27d81fe8b508f194d9322e6d58d2860bb6
Renato Madeia Muiambo <amlieogalaxias@gmail.com>
2026-04-29 07:25:30 +0200

    fix: adjust IAMAccess II

--- terraform/global/backend/backend.hcl
+++ terraform/global/backend/backend.hcl
@@ -1,5 +1,5 @@
 bucket = "s3-state-proji"
 key = "prod/terraform.tfstate"
 region = "us-east-1"
-dynamodb_table = "dynamodb-state-proji"
+use_lockfile = "dynamodb-state-proji"
 encrypt = true
--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -9,7 +9,7 @@ resource "aws_iam_role_policy_attachment" "ssm" {
 
 resource "aws_iam_role_policy_attachment" "ecr" {
   role = data.aws_iam_role.ec2_assume_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryOnly"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_iam_instance_profile" "iam_instance_profile" {
