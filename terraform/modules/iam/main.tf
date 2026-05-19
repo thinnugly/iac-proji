f766239ad4a4938453df26f5226daeb249098172
Renato Madeia Muiambo <amlieogalaxias@gmail.com>
2026-05-19 13:41:45 +0200

    fix(terraform) adjust EC2 permissions

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -14,15 +14,40 @@ resource "aws_iam_role" "ec2_assume_role" {
   })
 }
 
+# --- POLÍTICAS GERENCIADAS (AWS MANAGED) ---
+
 resource "aws_iam_role_policy_attachment" "ssm" {
-  role = aws_iam_role.ec2_assume_role.name
+  role       = aws_iam_role.ec2_assume_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "ecr" {
-  role = aws_iam_role.ec2_assume_role.name
+  role       = aws_iam_role.ec2_assume_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+# --- POLÍTICA INLINE CUSTOMIZADA PARA O ANSIBLE/PIPELINE ---
+
+resource "aws_iam_role_policy" "ssm_ansible_execution" {
+  name = "ssm_ansible_execution"
+  role = aws_iam_role.ec2_assume_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Effect = "Allow"
+        Action = [
+          "ssm:SendCommand",
+          "ssm:GetCommandInvocation",
+          "ssm:ListCommandInvocations",
+          "ssm:CancelCommand"
+        ]
+        Resource = "*"
+    }]
+  })
+}
+
+# --- INSTANCE PROFILE ---
 
 resource "aws_iam_instance_profile" "iam_instance_profile" {
   name = var.iam_instance_profile
